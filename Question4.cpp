0556b8298815ffc169df9aebdbe86bd8dc9ac178
Devashnee <devashneebhagawandin@hotmail.com>
2016-03-29 22:43:20 +0200

    q4.1-4.3

--- Question4.cpp
+++ Question4.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name: devashnee bhagawandin
+// Student No:214502393
+// Date: 2016-03-29
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ***** QUESTION 4 ****
@@ -11,3 +11,47 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+#include<iostream>
+#include<string>
+using namespace std;
+
+class animal{
+public:
+
+	virtual void name()=0;
+	virtual void does()=0;
+};
+
+class Bird :public virtual animal{
+	
+	void name(){
+		cout << " a bird";
+	}
+	void does(){
+		cout << "cherps" << endl;
+	}
+};
+
+
+class Dog :public virtual animal{
+	void name(){
+		cout << " a dog";
+	}
+	void does(){
+		cout << "barks" << endl;
+	}
+
+};
+
+int main(){
+	Dog d;
+	Bird b;
+	animal	*ptn1=&d; 
+	animal *ptn2 = &b;
+	ptn1->name();
+	ptn1->does();
+	ptn2->name();
+	ptn2->does();
+
+
+}
